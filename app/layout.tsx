bda4412e02cd741c5124fbf0dad3ac34e706065e
Dharmik Shah <103660203+dharmik173@users.noreply.github.com>
2025-03-08 08:00:25 +0000

    Initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,35 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "URL Shortener | Shorten & Share Links Instantly",
+  description:
+    "Shorten long URLs with ease using our simple and fast URL Shortener. Generate short, shareable links in seconds, track recent URLs, and copy with one click",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
