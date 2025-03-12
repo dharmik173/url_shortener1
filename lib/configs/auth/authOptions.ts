c3370c28392fb5c39afc3db95ddffbddb76489a5
dharmikshah189 <dharmikshah189@gmail.com>
2025-03-12 04:39:57 +0000

    add google auth signup ,add callback url for google.

--- lib/configs/auth/authOptions.ts
+++ lib/configs/auth/authOptions.ts
@@ -10,46 +10,62 @@ import  { type User } from "next-auth";
 
 
 export const authOptions:AuthOptions  = {
-    providers: [
-      // ✅ Google OAuth Provider
-      GoogleProvider({
-        clientId: process.env.GOOGLE_CLIENT_ID!,
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-      }),
-  
-      CredentialsProvider({
-        name: "Credentials",
-        credentials: {
-          email: { label: "Email", type: "text" },
-          password: { label: "Password", type: "password" },
-        },
-        async authorize(credentials) {
-          await connectToDB();
-  
-          const user = await UserData.findOne({ email: credentials?.email });
-          if (!user) throw new Error("Invalid credentials");
-  
-          const isPasswordValid = credentials?.password
-    ? await bcrypt.compare(credentials.password, user.password)
-    : false;
-          if (!isPasswordValid) throw new Error("Invalid credentials");
-  
-          return { id: user._id, name: user.name, email: user.email };
-        },
-      }),
-    ],
-    session: {
-      strategy: "jwt",
-    },
-    callbacks: {
-      async jwt({ token, user }: { token: JWT; user?: User }) {
-        if (user) token.user = user;
-        return token;
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      async profile(profile) {
+        await connectToDB();
+        let user = await UserData.findOne({ email: profile.email });
+        if (!user) {
+          user = await UserData.create({
+            name: profile.name,
+            email: profile.email,
+            password: "googleUser",
+            image: profile.picture,
+          });
+        }
+
+        return { id: user._id, name: user.name, email: user.email };
       },
-      async session({ session, token }: { session: Session; token: JWT }) {
-          session.user = token.user as Session["user"];
-        return session;
+    }),
+
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "text" },
+        password: { label: "Password", type: "password" },
       },
+      async authorize(credentials) {
+        await connectToDB();
+
+        const user = await UserData.findOne({ email: credentials?.email });
+        if (!user) throw new Error("Invalid credentials");
+
+        const isPasswordValid = credentials?.password
+          ? await bcrypt.compare(credentials.password, user.password)
+          : false;
+        if (!isPasswordValid) throw new Error("Invalid credentials");
+
+        return { id: user._id, name: user.name, email: user.email };
+      },
+    }),
+  ],
+  session: {
+    strategy: "jwt",
+  },
+  callbacks: {
+    async jwt({ token, user }: { token: JWT; user?: User }) {
+      if (user) token.user = user;
+      return token;
     },
-    secret: process.env.NEXTAUTH_SECRET,
-  };
+    async session({ session, token }: { session: Session; token: JWT }) {
+      session.user = token.user as Session["user"];
+      return session;
+    },
+    async redirect({ baseUrl }: { baseUrl: string }) {
+      return baseUrl;
+    },
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+};
